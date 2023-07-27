e2ab4b17e5c69e8e2c98ce271fd32914c6b39a93
Matvei Morenkov <morenmat@fel.cvut.cz>
2023-07-27 10:07:19 +0200

    initial commit

---
+++ src/main/java/pet/tasktrackerscheduler/TaskTrackerSchedulerApplication.java
@@ -0,0 +1,13 @@
+package pet.tasktrackerscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackerSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pet/tasktrackerscheduler/TaskTrackerSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package pet.tasktrackerscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
